abe9aad6a9acc9ceb08ce2fa110f9ec60b81bc75
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2018-06-25 11:58:55 +0100

    Redux store working for single channel app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,74 @@
-// TODO: add and export your own actions
+// const BASE_URL = 'https://wagon-chat.herokuapp.com';
+
+// export const FETCH_MESSAGES = 'FETCH_MESSAGES';
+// export const CREATE_MESSAGE = 'CREATE_MESSAGE';
+// export const CHANNEL_SELECTED = 'CHANNEL_SELECTED';
+
+// export function fetchMessages(channel) {
+//   const url = `${BASE_URL}/${channel}/messages`;
+//   const promise = fetch(url).then(result => result.json());
+
+//   return {
+//     type: FETCH_MESSAGES,
+//     payload: promise // Will be resolved by redux-promise
+//   };
+// }
+
+// export function createMessage(channel, user, message) {
+//   const body = { user, message };
+//   const url = `${BASE_URL}/${channel}/messages`;
+//   const promise = fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(body)
+//   }).then(r => r.json());
+
+//   return {
+//     type: CREATE_MESSAGE,
+//     payload: promise
+//   };
+// }
+
+const BASE_URL = 'https://wagon-chat.herokuapp.com';
+
+export const FETCH_MESSAGES = 'FETCH_MESSAGES';
+export const CREATE_MESSAGE = 'CREATE_MESSAGE';
+export const CHANNEL_SELECTED = 'CHANNEL_SELECTED';
+
+export function fetchMessages(channel) {
+  const url = `${BASE_URL}/${channel}/messages`;
+  const promise = fetch(url).then(r => r.json());
+
+  return {
+    type: FETCH_MESSAGES,
+    payload: promise // Will be resolved by redux-promise
+  };
+}
+
+export function createMessage(channel, author, content) {
+  const url = `${BASE_URL}/${channel}/messages`;
+  const body = { author, content }; // ES6 destructuring
+  const promise = fetch(url, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }).then(r => r.json());
+
+  return {
+    type: CREATE_MESSAGE,
+    payload: promise // Will be resolved by redux-promise
+  };
+}
+
+export function selectChannel(channel) {
+  return {
+    type: CHANNEL_SELECTED,
+    payload: channel
+  };
+}
---
+++ src/containers/message_form.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { createMessage } from '../actions/index';
+
+class MessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  componentDidMount() {
+    this.messageBox.focus();
+  }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.createMessage(this.props.selectedChannel, this.props.currentUser, this.state.value);
+    this.setState({ value: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          ref={(input) => { this.messageBox = input; }}
+          type="text"
+          placeholder="Message..."
+          className="form-control"
+          autoComplete="off"
+          onChange={this.handleChange}
+          value={this.state.value}
+        />
+        <button type="submit">Send</button>
+      </form>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ createMessage }, dispatch);
+}
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser,
+    selectedChannel: state.selectedChannel
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessageForm);
+
+// import React, { Component } from 'react';
+// import { bindActionCreators } from 'redux';
+// import { connect } from 'react-redux';
+// import { createMessage } from '../actions/index';
+
+// class MessageForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { value: '' };
+//   }
+
+//   componentDidMount() {
+//     this.messageBox.focus();
+//   }
+
+//   handleChange = (event) => {
+//     this.setState({ value: event.target.value });
+//   }
+
+//   handleSubmit = (event) => {
+//     event.preventDefault();
+//     this.props.createMessage(this.props.selectedChannel, this.props.currentUser, this.state.value);
+//     this.setState({ value: '' }); // Reset message input
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit} className="channel-editor">
+//         <input
+//           ref={(input) => { this.messageBox = input; }}
+//           type="text"
+//           className="form-control"
+//           autoComplete="off"
+//           value={this.state.value}
+//           onChange={this.handleChange}
+//         />
+//         <button type="submit">Send</button>
+//       </form>
+//     );
+//   }
+// }
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ createMessage }, dispatch);
+// }
+
+// function mapStateToProps(state) {
+//   return {
+//     currentUser: state.currentUser,
+//     selectedChannel: state.selectedChannel
+//   };
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(MessageForm);
